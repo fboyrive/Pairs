8614de1403dd20c57942327dce8cc5c52d0bf92d
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-03-27 12:45:44 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,20 +1,7 @@
-public class Card {
-	private static String possibleLetters = "abcdefghijklmnopqrstuvwxyz";
-    	private String cardString;
-    	private boolean hidden;
-    
-    	Card () {
-		int randomNumber = (int) (Math.random() * (possibleLetters.length()));
-		char character = possibleLetters.charAt(randomNumber);
-	    	this.cardString = Character.toString(character);
-		this.hidden = true;
-		possibleLetters = possibleLetters.substring(0, randomNumber) + possibleLetters.substring(randomNumber+1);
-    	}
-    
-	Card (String cardString) {
-		this.cardString = cardString;
-		this.hidden = true;
-	}
+public abstract class Card {
+    	protected String cardString;
+    	protected boolean hidden;
+    	
     
     	public boolean isHidden () {
         	return hidden;
@@ -24,7 +11,7 @@ public class Card {
     		this.hidden = true;   
     	}
 	
-	public void discover () {
+    	public void discover () {
     		this.hidden = false;   
     	}
 
