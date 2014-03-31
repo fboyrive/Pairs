f22ca69e4ae546a46527eb70eccd6a8d327ab375
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-03-31 22:29:24 +0800

    Update LatinWord.java

--- LatinWord.java
+++ LatinWord.java
@@ -1,45 +1,45 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class LatinWord extends Card {
-        static int i=0, z=0, MAX_WORDS=100, wordsUsed=0;
+        static int MAX_WORDS=100;
+        static int lineCount=0;
         static String [] word_list= new String[MAX_WORDS];
-        static int [] used_indexes= new int[MAX_WORDS];
-	
-        LatinWord () {
-        int lineCount=0,randomNumber;
-        String filename = "Dictionary.txt";
-        File file = new File(filename);
-        Scanner scan;
-        
-        try {
-            scan = new Scanner(file);
-            do{
-            word_list[i]= scan.nextLine();
-            i++;
-            lineCount++;
-            }while (scan.hasNextLine());
-            scan.close(); 
-            
-           } catch (FileNotFoundException ex) {
-            Logger.getLogger(LatinWord.class.getName()).log(Level.SEVERE, null, ex);
+
+        LatinWord () {	        
+	        // First initialisation
+        	if (lineCount == 0) {
+		        String filename = "Dictionary.txt";
+		        File file = new File(filename);
+		        Scanner scan;
+		        
+		        try {
+		            scan = new Scanner(file);
+		            do {
+			            word_list[lineCount]= scan.nextLine();
+			            lineCount++;
+		            } while (scan.hasNextLine());
+		            scan.close();
+		            
+		        } catch (FileNotFoundException ex) {
+		        	System.out.println(ex.getMessage()); 
+		        }
+        	}
+	        
+	        int randomNumber = (int) (Math.random() * lineCount);
+	        
+	        while (word_list[randomNumber].equals("this word has been used"))
+	        	randomNumber = (int) (Math.random() * lineCount);
+	        
+	        super.cardString = word_list[randomNumber];
+	        word_list[randomNumber]="this word has been used";
+	        super.hidden=true;
         }
-        
-        randomNumber = (int) (Math.random() * lineCount);
-        while (word_list[randomNumber].equals("this word has been used")) randomNumber = (int) (Math.random() * lineCount);
-        
-        super.cardString = word_list[randomNumber];
-        word_list[randomNumber]="this word has been used";
-        super.hidden=true;
-        }
-	
-	LatinWord (String cardString) {
-		super.cardString = cardString;
-		super.hidden = true;
-	}
-   
+
+		LatinWord (String cardString) {
+			super.cardString = cardString;
+			super.hidden = true;
+		}
 }
