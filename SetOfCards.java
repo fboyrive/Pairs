278649577f866be9a57f86c99b0ae6ad594ec578
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-04-04 20:04:21 +0800

    Update SetOfCards.java

--- SetOfCards.java
+++ SetOfCards.java
@@ -1,29 +1,28 @@
 public class SetOfCards {
 
-    	final static int cardsPerLine = 4;
-    	final static int cardsPerRow = 4;
-    	private Card[][] card_matrix = new Card[cardsPerLine][cardsPerRow];
-        static int[] numberOfTries = new int[3], numberOfSuccess= new int[3], numberOfFailures= new int[3]; //[1]=p1, [2]=p2, just set to 3 values for easy of reading
+    	private Card[][] card_matrix = new Card[Constants.cardsPerColumn][Constants.cardsPerRow];
     	private Card[] tempCard = new Card[2];
     	private boolean clock;
-        static int player= 1;
-        static boolean twoPlayers= true;
+    	static int[] numberOfTries = new int[3], numberOfSuccess= new int[3], numberOfFailures= new int[3]; //[1]=p1, [2]=p2, just set to 3 values for easy of reading
+    	static int player= 1;
+    	static boolean twoPlayers= true;
+
+
     
     	SetOfCards (int option) {
     		// option == 1 => SetOfLatinLetters
     		// option == 2 => SetOfLatinWords
     		// ...
 
-	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-		    	for(int j=0 ; j<cardsPerLine ; j=j+2) {
+	    	for (int i=0 ; i<Constants.cardsPerColumn ; i++) {   
+		    	for(int j=0 ; j<Constants.cardsPerRow ; j=j+2) {
 		    		if (option == 1) {
 		    			card_matrix[i][j]= new LatinLetter();
 		    			card_matrix[i][j+1]= new LatinLetter (card_matrix[i][j].getCardString());
 		    		}
-		    		else if (option==2) {
+		    		else if (option == 2) {
 		    			card_matrix[i][j]= new LatinWord();
 		    			card_matrix[i][j+1]= new LatinWord (card_matrix[i][j].getCardString());
-                                        
 		    		}
 		    	}
 	    	}
@@ -40,18 +39,18 @@ public class SetOfCards {
     	 */
 	    	int rand;
 	    	Card temp;
-	    	for(int i= cardsPerLine*cardsPerRow -1 ; i>=2 ; i--) {
+	    	for(int i= Constants.cardsPerColumn*Constants.cardsPerRow -1 ; i>=2 ; i--) {
 	    		rand = (int) (Math.random() * (i+1));
-	    		temp = card_matrix[(int) i/cardsPerLine][i%cardsPerLine];
-	    		card_matrix[(int) i/cardsPerLine][i%cardsPerLine]=card_matrix[(int) rand/cardsPerLine][rand%cardsPerLine];
-	    		card_matrix[(int) rand/cardsPerLine][rand%cardsPerLine] = temp;
+	    		temp = card_matrix[(int) i/Constants.cardsPerColumn][i%Constants.cardsPerRow];
+	    		card_matrix[(int) i/Constants.cardsPerColumn][i%Constants.cardsPerRow]=card_matrix[(int) rand/Constants.cardsPerColumn][rand%Constants.cardsPerRow];
+	    		card_matrix[(int) rand/Constants.cardsPerColumn][rand%Constants.cardsPerRow] = temp;
 	    	}
     	}
     
     	public String[][] getMatrix() {
-	    	String[][] matrix = new String [cardsPerLine][cardsPerRow];
-	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-		    	for(int j=0 ; j<cardsPerLine ; j++) {
+	    	String[][] matrix = new String [Constants.cardsPerColumn][Constants.cardsPerRow];
+	    	for (int i=0 ; i<Constants.cardsPerColumn ; i++) {   
+		    	for(int j=0 ; j<Constants.cardsPerRow ; j++) {
 		    		if (card_matrix[i][j].isHidden())
 		    			matrix[i][j]="null";
 		    		else
@@ -61,13 +60,12 @@ public class SetOfCards {
 	    	return matrix;
     	}
     
-    
     	public boolean win() {
-    		return (this.numberOfSuccess[1]==(cardsPerLine*cardsPerRow/2));
+    		return (numberOfSuccess[1]==(Constants.cardsPerColumn*Constants.cardsPerRow/2));
     	}
       
     	public boolean click1 (int x, int y) {
-    		if (x<0 || x>= cardsPerLine || y<0 || y>=cardsPerRow)
+    		if (x<0 || x>= Constants.cardsPerColumn || y<0 || y>=Constants.cardsPerRow)
     			return false;
     		else if (!card_matrix[x][y].isHidden())
     			return false;
@@ -79,31 +77,34 @@ public class SetOfCards {
     	}
     	
     	public boolean click2 (int x, int y) {
-    		if (x<0 || x>= cardsPerLine || y<0 || y>=cardsPerRow)
+    		if (x<0 || x>= Constants.cardsPerColumn || y<0 || y>=Constants.cardsPerRow)
     			return false;
     		else if (!card_matrix[x][y].isHidden())
     			return false;
     		else {
 				card_matrix[x][y].discover();
-    			
-                        numberOfTries[player]++;
-    			
-                        if (card_matrix[x][y].getCardString().compareTo(tempCard[0].getCardString())==0) {
+    			numberOfTries[player]++;
+    			if (card_matrix[x][y].getCardString().compareTo(tempCard[0].getCardString())==0) {
     				tempCard[0]=null;
     				numberOfSuccess[player]++;
     			}
     			else {
     				clock=true;
     				tempCard[1]=card_matrix[x][y];
-                                numberOfFailures[player]++;
-                                if(twoPlayers==true) changePlayer();
-                        }
+    				numberOfFailures[player]++;
+    				if(twoPlayers==true) changePlayer();
+    			}
     			return true;
     		}
     	}
     	
     	public boolean getClock() {
     		return clock;
+    	}
+    	
+    	private void changePlayer() {
+    		if (player==1) player=2;
+    		else player=1; 
     	}
     	
     	public void endClock() {
@@ -116,8 +117,8 @@ public class SetOfCards {
     
     	public String toString() {
 	    	String print="";
-	    	for (int i=0 ; i<cardsPerRow ; i++) {   
-		    	for(int j=0 ; j<cardsPerLine ; j++) {
+	    	for (int i=0 ; i<Constants.cardsPerColumn ; i++) {   
+		    	for(int j=0 ; j<Constants.cardsPerRow ; j++) {
 		    		if (card_matrix[i][j].isHidden())
 		    		print= print + "Card ("+i + ","+ j+ "): "+ card_matrix[i][j].getCardString()+" (hidden)\n";
 		    		else
@@ -126,9 +127,4 @@ public class SetOfCards {
 	    	}
 	    	return print;
     	}
-    
-    private void changePlayer() {
-        if (player==1)player=2;
-        else player=1; 
-    }
 }
