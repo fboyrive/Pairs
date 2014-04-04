d2938c667adfd4d316aa95d3058695b9e4f225c6
fboyrive <florian.boyrivent@telecom-bretagne.eu>
2014-04-04 20:05:38 +0800

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+public class Constants {
+	// GUI Constants
+	final static int windowHeight = 630;
+	final static int windowWidth = 650;
+	
+	final static Color gameBackgroundColor = Color.GRAY;
+	final static int cardImageSize = 142;
+	final static int cardSize = 160; // = cardImageSize + margin
+	final static String fontGame = "Courier";
+	final static int cardsFontSize = 30;
+	final static Color cardsFontColor = Color.RED;
+	
+	// Other Constants
+	final static int wrongTryWaitingTime = 2000; // in milisecondes
+	final static int cardsPerRow = 4;
+	final static int cardsPerColumn = 4;
+	
+	final static int MaxLinesFile = 100;
+
+}
